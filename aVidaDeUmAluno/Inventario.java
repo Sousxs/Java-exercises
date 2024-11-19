2ff3a95e11c6ba09c9606202318281335b3dd2c4
João Pedro <167132754+jhonzito66@users.noreply.github.com>
2024-11-19 17:49:22 -0300

    Add files via upload

---
+++ aVidaDeUmAluno/Inventario.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Inventario {
+    private List<Item> itens;
+    private static final int CAPACIDADE_MAXIMA = 3; // definir tamanho do inventario
+
+    public Inventario() {
+        this.itens = new ArrayList<>();
+    }
+
+    public boolean adicionarItem(Item item) {
+        if (itens.size() >= CAPACIDADE_MAXIMA) {
+            return false;
+        } else {
+            itens.add(item);
+            return true;
+        }
+    }
+
+    public Item removerItem(String nomeItem) {
+        for (Item item : itens) {
+            if (item.getNome().equalsIgnoreCase(nomeItem)) {
+                itens.remove(item);
+                return item;
+            }
+        }
+        System.out.println(nomeItem + " não está no inventário.");
+        return null;
+    }
+
+    public void listarItens() {
+        if (itens.isEmpty()) {
+            System.out.println("O inventário está vazio.");
+        } else {
+            System.out.println("Itens no inventário:");
+            for (Item item : itens) {
+                System.out.println("- " + item.getNome());
+            }
+        }
+    }
+
+    public boolean possuiItem(String nomeItem) {
+        for (Item item : itens) {
+            if (item.getNome().equalsIgnoreCase(nomeItem)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ aVidaDeUmAluno/Npc.java
@@ -0,0 +1,30 @@
+public class Npc {
+    private String nome;
+    private String conversa;
+
+    public Npc(String nome, String conversa) {
+        this.nome = nome;
+        this.conversa = conversa;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getConversa() {
+        return conversa;
+    }
+
+    public void setConversa(String conversa) {
+        this.conversa = conversa;
+    }
+
+    public void Conversar() {
+        System.out.println(this.nome + " diz: " + this.conversa);
+    }
+
+}
