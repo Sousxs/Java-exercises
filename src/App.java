09d6b25185c1f3766585cde3a81e17afd128a82d
Pedro H. <162902128+Sousxs@users.noreply.github.com>
2025-06-11 16:01:49 -0300

    update

--- src/App.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class App  {
-    public static void main(String[] args) throws Exception {
-        List<String> nomes = Arrays.asList("João", "Maria", "José", "Ana", "Pedro");
-    
-        nomes.forEach(nome -> System.out.println("Olá, " + nome + "!"));
-    }
-}
