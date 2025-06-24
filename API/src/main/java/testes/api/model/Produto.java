d80c186db1e3494b8db220eb1ca544cb8974bd25
Pedro H. <162902128+Sousxs@users.noreply.github.com>
2025-06-24 17:35:44 -0300

    spring

--- API/src/main/java/testes/api/model/Produto.java
+++ API/src/main/java/testes/api/model/Produto.java
@@ -5,5 +5,12 @@ public class Produto {
     private String nome;
     private double preco;
 
-    // Getters e setters
+    public Produto() {} // Construtor padrão necessário para desserialização
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public double getPreco() { return preco; }
+    public void setPreco(double preco) { this.preco = preco; }
 }
